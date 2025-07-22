21a2918ca910d25f589680eece4ec9a798586b0f
kesavaprasath2007 <kesavaprasath72@gmail.com>
2025-07-22 17:19:37 +0530

    Create stack using list.py
    Signed-off-by: kesavaprasath2007 <kesavaprasath72@gmail.com>

---
+++ stack using list.py
@@ -0,0 +1,15 @@
+stack=[]
+
+stack.append("arun")
+stack.append("alli")
+stack.append("kumar")
+stack.append("mani")
+stack.append("muthu")
+
+print("stack:",stack)
+
+popped_item1=stack.pop()
+popped_item2=stack.pop()
+print("popped:",popped_item1,popped_item2)
+
+print("current stack:",stack)
